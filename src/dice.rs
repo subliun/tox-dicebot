b1bb9883dd5705082242867320a130fe71bfa910
subliun <subliunisdev@gmail.com>
2015-01-17 04:08:38 +1030

    applied patch from whoever (after much struggle)

---
+++ src/dice.rs
@@ -0,0 +1,90 @@
+use std::rand;
+
+pub fn get_response_flip(user_name: String) -> String {
+  user_name + "'s coin landed on " + if rand::random::<bool>() == true { "heads." } else { "tails." }
+}
+
+pub fn get_response_dice_roll(message: &str, user_name: String) -> String {
+  let param = if message == "" { "6" } else { message };
+
+  match param {
+    "joint" => return "smoke weed everyday".to_string(),
+      "rick" => return "never gonna give you up".to_string(),
+      _ => { },
+  }
+
+  if param.contains(" 0") || param.contains("d0") {
+    return "Error Dividing By Cucumber. Reinstall Universe And Try Again.".to_string();
+  }
+
+  let mut roll_result = None;
+  let times;
+  let roll_range;
+  if param.contains("d") {
+    let d_location = param.find('d').unwrap();
+    times = param.slice_to(d_location).replace("d", "").parse::<u32>().unwrap_or(1);
+    if times > 500 {
+      return "Invalid request. You tried to roll too many times. My robot arms can only take so much. ;_;'".to_string()
+    }
+    roll_range = param.slice_from(d_location).replace("d", "").parse::<u64>();
+  } else {
+    times = 1;
+    roll_range = param.parse::<u64>();
+  }
+
+  let mut roll_sum = 0;
+  let mut result_builder = String::new();
+  if roll_range != None && roll_range.unwrap() > 0 {
+    for i in range(0, times) {
+      if i != 0 { result_builder.push_str(", ") };
+      if i == times - 1 && times > 1 { result_builder.push_str("and "); }
+      let roll = roll_dice(roll_range.unwrap());
+      roll_sum += roll;
+      result_builder.push_str(add_formatting(roll).as_slice());
+    }
+    roll_result = Some(result_builder);
+  }
+
+  if roll_result == None {
+    on_invalid_input(user_name)
+  } else {
+    if times == 1 {
+      user_name + " rolled " + roll_result.unwrap().as_slice()
+    } else {
+      user_name + " rolled a total of " + roll_sum.to_string().as_slice() + " with rolls of " + roll_result.unwrap().as_slice()
+    }
+  }
+}
+
+fn on_invalid_input(user_name: String) -> String {
+  match &*user_name {
+    "{☯}S☠ǚll{☣}" => "Invalid request MORON. Please use your GAY head to type non-fucking-negative numbers.",
+    "Candy Gumdrop" => "Invalid request GORGEOUS. Please use your BEAUTIFUL head to type non-fucking-negative numbers.",
+    _ => "Invalid request. Please use a non-negative number between 2 and 2^64.",
+  }.to_string()
+}
+
+fn roll_dice(roll_range: u64) -> u64 {
+  ((rand::random::<u64>() % roll_range) + 1)
+}
+
+fn add_formatting(roll: u64) -> String {
+  let die_face: String = match get_die_face(roll) {
+    Some(face) => " ".to_string() + face.to_string().as_slice(),
+      None => "".to_string(),
+  };
+
+  roll.to_string() + die_face.as_slice()
+}
+
+fn get_die_face(number: u64) -> Option<char> {
+  match number {
+    1 => Some('⚀'),
+      2 => Some('⚁'),
+      3 => Some('⚂'),
+      4 => Some('⚃'),
+      5 => Some('⚄'),
+      6 => Some('⚅'),
+      _ => None
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -24,10 +24,12 @@ use markov::Chain;
 
 use std::slice::SliceExt;
 use std::sync::mpsc::{Select};
-use std::io::Timer;
+use std::io::timer::{self, Timer};
 use std::time::Duration;
 
 mod battle;
+mod dice;
+mod zalgo;
 
 static BOOTSTRAP_IP: &'static str = "192.254.75.102";
 static BOOTSTRAP_PORT: u16 = 33445;
@@ -36,6 +38,77 @@ static BOOTSTRAP_KEY: &'static str =
 static GROUPCHAT_ADDR: &'static str =
 "56A1ADE4B65B86BCD51CC73E2CD4E542179F47959FE3E0E21B4B0ACDADE51855D34D34D37CB5";
 static BOT_NAME: &'static str = "DiceBot";
+
+// consider incapsulating this into a separate entity
+fn do_msg(tox: &Tox, battle: &mut battle::Battle, chain: &mut Chain<String>, group: i32, peer: i32, msg: String) {
+  let mut mit = msg.splitn(1, ' ');
+  match mit.next().unwrap() {
+    "^diceid" => {
+      //tox.group_message_send(group, "My Tox ID is: ".to_string() + tox.get_address().to_string().as_slice());
+    },
+    "^dice" => {
+      let user_name = tox.group_peername(group, peer).unwrap();
+      let roll = dice::get_response_dice_roll(mit.next().unwrap_or(""), user_name);
+      // TODO: add a `split_send` function
+      for reply in tox::util::split_message(roll.as_slice()).iter() {
+        tox.group_message_send(group, reply.to_string());
+        timer::sleep(Duration::milliseconds(500));
+      }
+    },
+    "^flip" => {
+      let user_name = tox.group_peername(group, peer).unwrap();
+      tox.group_message_send(group, dice::get_response_flip(user_name));
+    },
+    "^zalgo" => {
+      let zalgo = zalgo::make_zalgo(mit.next().unwrap_or("").trim().to_string());
+      for reply in tox::util::split_message(zalgo.as_slice()).iter() {
+        tox.group_message_send(group, reply.to_string());
+        timer::sleep(Duration::milliseconds(200));
+      }
+    },
+    "^question" => {
+      tox.group_message_send(group, question::retrieve_answer(mit.next().unwrap_or("").trim().to_string()));
+    },
+    "^youtube" => {
+      //tox.group_message_send(group, "NOT YET IMPLEMENTED".to_string());
+      //youtube::play_audio_file(&av, group, "downloaded_videos/az.pcm".to_string())
+    },
+    "^fight" => {
+      tox.group_message_send(group, fight::get_response_fight(mit.next().unwrap_or("").trim().to_string()));
+    },
+    /*"batssftle" => {
+      let mut names: Vec<String> = Vec::new();
+      for name in tox.group_get_names(group).unwrap().into_iter() {
+        if name.is_some() {
+          names.push(name.unwrap());
+        }
+      }
+
+      battle.start_battle(tox, group, names);
+      //battle_timer = Some(Timer::new().unwrap().oneshot(Duration::seconds(battle.duration as i64)));
+    },*/
+    "^endchat" => {
+      tox.set_name("DiceBot".to_string()).unwrap();
+    },
+    "^chat" => {
+      tox.set_name("David Cameron".to_string()).unwrap();
+      tox.group_message_send(group, chain.generate_str());
+    },
+    "^remember" => {
+      let result = remember::remember_assoc(mit.next().unwrap_or("").to_string());
+      if result != "" {
+        tox.group_message_send(group, result);
+      }
+    },
+    _ if msg.starts_with("^") => {
+      let result = remember::retrieve_assoc(mit.next().unwrap_or("").to_string());
+      if result != None {
+        tox.group_message_send(group, result.unwrap());
+      }
+    },
+    _ => {},
+  }
+}
 
 fn main() {
   let tox = Tox::new(ToxOptions::new()).unwrap();
@@ -61,7 +134,7 @@ fn main() {
   println!("My address is: {:?}", tox.get_address());
 
   let mut battle = battle::Battle::new();
-  let mut battle_timer = None;
+  //let mut battle_timer = None;
 
   let mut chain = Chain::for_strings();
   chain.feed_file(&Path::new("markov.txt"));
@@ -81,74 +154,25 @@ fn main() {
           tox.add_friend_norequest(friend_id);
         },
 
-          GroupInvite(id, ty, data) => {
-            println!("GroupInvite(_, {:?}, _) ", ty);
-            match ty {
-              GroupchatType::Text => tox.join_groupchat(id, data).unwrap(),
-                GroupchatType::Av => av.join_av_groupchat(id, data).unwrap(),
-            };
-          },
+        GroupInvite(id, ty, data) => {
+          println!("GroupInvite(_, {:?}, _) ", ty);
+          match ty {
+            GroupchatType::Text => tox.join_groupchat(id, data).unwrap(),
+              GroupchatType::Av => av.join_av_groupchat(id, data).unwrap(),
+          };
+        },
 
-          GroupMessage(group, peer, msg) => if tox.group_peername(group, peer).unwrap() != tox.get_self_name().unwrap() {
-            println!("{}: {}", tox.group_peername(group, peer).unwrap(), msg);
+        GroupMessage(group, peer, msg) => if tox.group_peername(group, peer).unwrap() != tox.get_self_name().unwrap() {
+          println!("{}: {}", tox.group_peername(group, peer).unwrap(), msg);
 
-            if msg.starts_with("^") && !msg.starts_with("^chat") {
-                tox.set_name(BOT_NAME.to_string()).unwrap();
-            }
+          if msg.starts_with("^") && !msg.starts_with("^chat") {
+              tox.set_name(BOT_NAME.to_string()).unwrap();
+          }
 
-            if msg.starts_with("^diceid") {
-              //tox.group_message_send(group, "My Tox ID is: ".to_string() + tox.get_address().to_string().as_slice());
-            } else if msg.starts_with("^dice") || msg.starts_with("^roll") {
-              let user_name = tox.group_peername(group, peer).unwrap();
-              for reply in tox::util::split_message(dice::get_response_dice_roll(msg, user_name).as_slice()).iter() {
-                tox.group_message_send(group, reply.to_string());
-                std::io::timer::sleep(std::time::duration::Duration::milliseconds(500));
-              }
-            } else if msg.starts_with("^flip") {
-              let user_name = tox.group_peername(group, peer).unwrap();
+          do_msg(&tox, &mut battle, &mut chain, group, peer, msg);
+        },
 
-              tox.group_message_send(group, dice::get_response_flip(user_name));
-            } else if msg.starts_with("^zalgo") {
-              for reply in tox::util::split_message(zalgo::make_zalgo(msg.replace("^zalgo", "")).trim().as_slice()).iter() {
-                tox.group_message_send(group, reply.to_string());
-                std::io::timer::sleep(std::time::duration::Duration::milliseconds(200));
-              }
-            } else if msg.starts_with("^question") {
-              tox.group_message_send(group, question::retrieve_answer(msg.replace("^question", "").trim().to_string()));
-            } else if msg.starts_with("^youtube") {
-              //tox.group_message_send(group, "NOT YET IMPLEMENTED".to_string());
-              //youtube::play_audio_file(&av, group, "downloaded_videos/az.pcm".to_string())
-            } else if msg.starts_with("^fight") {
-              tox.group_message_send(group, fight::get_response_fight(msg.replace("^fight", "").trim().to_string()));
-            } else if msg.starts_with("batssftle") {
-              let mut names: Vec<String> = vec!();
-              for name in tox.group_get_names(group).unwrap().into_iter() {
-                if name.is_some() {
-                  names.push(name.unwrap());
-                }
-              }
-
-              battle.start_battle(&tox, group, names);
-              battle_timer = Some(Timer::new().unwrap().oneshot(Duration::seconds(battle.duration as i64)));
-            } else if msg.starts_with("^endchat") {
-              tox.set_name("DiceBot".to_string()).unwrap();
-            } else if msg.starts_with("^chat") {
-              tox.set_name("David Cameron".to_string()).unwrap();
-              tox.group_message_send(group, chain.generate_str());
-            } else if msg.starts_with("^remember") {
-              let result = remember::remember_assoc(msg.replace("^remember", ""));
-              if result != "" {
-                tox.group_message_send(group, result);
-              }
-            } else if msg.starts_with("^") {
-              let result = remember::retrieve_assoc(msg.replace("^", ""));
-              if result != None {
-                tox.group_message_send(group, result.unwrap());
-              }
-            }
-          },
-
-          _ => { }
+        _ => { }
       }
     }
   }
@@ -242,7 +266,7 @@ mod remember {
 
     if path.exists() {
       file = File::open(&path);
-    }else {
+    } else {
       return None
     }
 
@@ -258,158 +282,6 @@ mod remember {
     }
 
     return result
-  }
-}
-
-mod dice {
-  use std::rand;
-
-  pub fn get_response_flip(user_name: String) -> String {
-    user_name + "'s coin landed on " + if rand::random::<bool>() == true { "heads." } else { "tails." }
-  }
-
-  pub fn get_response_dice_roll(message: String, user_name: String) -> String {
-    let m_param = message.split(' ').nth(1);
-
-    let param = m_param.unwrap_or("6");
-
-    match param {
-      "joint" => return "smoke weed everyday".to_string(),
-        "rick" => return "never gonna give you up".to_string(),
-        _ => { },
-    }
-
-    if param.contains(" 0") || param.contains("d0") {
-      return "Error Dividing By Cucumber. Reinstall Universe And Try Again.".to_string();
-    }
-
-    let mut roll_result = None;
-    let times;
-    let roll_range;
-    if param.contains("d") {
-      let d_location = param.find('d').unwrap();
-      times = param.slice_to(d_location).replace("d", "").parse::<u32>().unwrap_or(1);
-      if times > 500 {
-        return "Invalid request. You tried to roll too many times. My robot arms can only take so much. ;_;'".to_string()
-      }
-      roll_range = param.slice_from(d_location).replace("d", "").parse::<u64>();
-    } else {
-      times = 1;
-      roll_range = param.parse::<u64>();
-    }
-
-    let mut roll_sum = 0;
-    let mut result_builder = String::new();
-    if roll_range != None && roll_range.unwrap() > 0 {
-      for i in range(0, times) {
-        if i != 0 { result_builder.push_str(", ") };
-        if i == times - 1 && times > 1 { result_builder.push_str("and "); }
-        let roll = roll_dice(roll_range.unwrap());
-        roll_sum += roll;
-        result_builder.push_str(add_formatting(roll).as_slice());
-      }
-      roll_result = Some(result_builder);
-    }
-
-    if roll_result == None {
-      on_invalid_input(user_name)
-    } else {
-      if times == 1 {
-        user_name + " rolled " + roll_result.unwrap().as_slice()
-      } else {
-        user_name + " rolled a total of " + roll_sum.to_string().as_slice() + " with rolls of " + roll_result.unwrap().as_slice()
-      }
-    }
-  }
-
-  fn on_invalid_input(user_name: String) -> String {
-    if user_name == "{☯}S☠ǚll{☣}" {
-      "Invalid request MORON. Please use your GAY head to type non-fucking-negative numbers.".to_string()
-    } else if user_name == "Candy Gumdrop" {
-      "Invalid request GORGEOUS. Please use your BEAUTIFUL head to type non-fucking-negative numbers.".to_string()
-    } else {
-      "Invalid request. Please use a non-negative number between 2 and 2^64.".to_string()
-    }
-  }
-
-  fn roll_dice(roll_range: u64) -> u64 {
-    ((rand::random::<u64>() % roll_range) + 1)
-  }
-
-  fn add_formatting(roll: u64) -> String {
-    let die_face: String = match get_die_face(roll) {
-      Some(face) => " ".to_string() + face.to_string().as_slice(),
-        None => "".to_string(),
-    };
-
-    roll.to_string() + die_face.as_slice()
-  }
-
-  fn get_die_face(number: u64) -> Option<char> {
-    match number {
-      1 => Some('⚀'),
-        2 => Some('⚁'),
-        3 => Some('⚂'),
-        4 => Some('⚃'),
-        5 => Some('⚄'),
-        6 => Some('⚅'),
-        _ => None
-    }
-  }
-
-}
-
-mod zalgo {
-  use std::rand;
-  use std::rand::{thread_rng, Rng};
-
-  static ZALGO_CHARS: [char; 113]  = [
-    '\u{30d}', /*     Ì     */		 '\u{30e}', /*     ÌŽ     */	 '\u{304}', /*     Ì„     */	 '\u{305}', /*     Ì…     */
-    '\u{33f}', /*     Ì¿     */		'\u{311}', /*     Ì‘     */		'\u{306}', /*     Ì†     */		'\u{310}', /*     Ì     */
-    '\u{352}', /*     Í’     */		'\u{357}', /*     Í—     */		'\u{351}', /*     Í‘     */		'\u{307}', /*     Ì‡     */
-    '\u{308}', /*     Ìˆ     */		'\u{30a}', /*     ÌŠ     */		'\u{342}', /*     Í‚     */		'\u{343}', /*     Ì“     */
-    '\u{344}', /*     ÌˆÌ     */	 '\u{34a}', /*     ÍŠ     */	 '\u{34b}', /*     Í‹     */	 '\u{34c}', /*     ÍŒ     */
-    '\u{303}', /*     Ìƒ     */		'\u{302}', /*     Ì‚     */		'\u{30c}', /*     ÌŒ     */		'\u{350}', /*     Í     */
-    '\u{300}', /*     Ì€     */		'\u{301}', /*     Ì     */		 '\u{30b}', /*     Ì‹     */	 '\u{30f}', /*     Ì     */
-    '\u{312}', /*     Ì’     */		'\u{313}', /*     Ì“     */		'\u{314}', /*     Ì”     */		'\u{33d}', /*     Ì½     */
-    '\u{309}', /*     Ì‰     */		'\u{363}', /*     Í£     */		'\u{364}', /*     Í¤     */		'\u{365}', /*     Í¥     */
-    '\u{366}', /*     Í¦     */		'\u{367}', /*     Í§     */		'\u{368}', /*     Í¨     */		'\u{369}', /*     Í©     */
-    '\u{36a}', /*     Íª     */		'\u{36b}', /*     Í«     */		'\u{36c}', /*     Í¬     */		'\u{36d}', /*     Í­     */
-    '\u{36e}', /*     Í®     */		'\u{36f}', /*     Í¯     */		'\u{33e}', /*     Ì¾     */		'\u{35b}', /*     Í›     */
-    '\u{346}', /*     Í†     */		'\u{31a}', /*     Ìš     */
-    '\u{316}', /*     Ì–     */		'\u{317}', /*     Ì—     */		'\u{318}', /*     Ì˜     */		'\u{319}', /*     Ì™     */
-    '\u{31c}', /*     Ìœ     */		'\u{31d}', /*     Ì     */		 '\u{31e}', /*     Ìž     */	 '\u{31f}', /*     ÌŸ     */
-    '\u{320}', /*     Ì      */		'\u{324}', /*     Ì¤     */		'\u{325}', /*     Ì¥     */		'\u{326}', /*     Ì¦     */
-    '\u{329}', /*     Ì©     */		'\u{32a}', /*     Ìª     */		'\u{32b}', /*     Ì«     */		'\u{32c}', /*     Ì¬     */
-    '\u{32d}', /*     Ì­     */		 '\u{32e}', /*     Ì®     */	 '\u{32f}', /*     Ì¯     */	 '\u{330}', /*     Ì°     */
-    '\u{331}', /*     Ì±     */		'\u{332}', /*     Ì²     */		'\u{333}', /*     Ì³     */		'\u{339}', /*     Ì¹     */
-    '\u{33a}', /*     Ìº     */		'\u{33b}', /*     Ì»     */		'\u{33c}', /*     Ì¼     */		'\u{345}', /*     Í…     */
-    '\u{347}', /*     Í‡     */		'\u{348}', /*     Íˆ     */		'\u{349}', /*     Í‰     */		'\u{34d}', /*     Í     */
-    '\u{34e}', /*     ÍŽ     */		'\u{353}', /*     Í“     */		'\u{354}', /*     Í”     */		'\u{355}', /*     Í•     */
-    '\u{356}', /*     Í–     */		'\u{359}', /*     Í™     */		'\u{35a}', /*     Íš     */		'\u{323}', /*     Ì£     */
-    '\u{315}', /*     Ì•     */		'\u{31b}', /*     Ì›     */		'\u{340}', /*     Ì€     */		'\u{341}', /*     Ì     */
-    '\u{358}', /*     Í˜     */		'\u{321}', /*     Ì¡     */		'\u{322}', /*     Ì¢     */		'\u{327}', /*     Ì§     */
-    '\u{328}', /*     Ì¨     */		'\u{334}', /*     Ì´     */		'\u{335}', /*     Ìµ     */		'\u{336}', /*     Ì¶     */
-    '\u{34f}', /*     Í     */		 '\u{35c}', /*     Íœ     */	 '\u{35d}', /*     Í     */		'\u{35e}', /*     Íž     */
-    '\u{35f}', /*     ÍŸ     */		'\u{360}', /*     Í      */		'\u{362}', /*     Í¢     */		'\u{338}', /*     Ì¸     */
-    '\u{337}', /*     Ì·     */		'\u{361}', /*     Í¡     */		'\u{489}' /*     Ò‰_     */
-      ];
-
-  pub fn make_zalgo(input: String) -> String {
-    let mut result: String = String::new();
-    for character in input.chars() {
-      result.push_str(character.to_string().as_slice());
-
-      if character == ' ' {
-        continue;
-      }
-
-      for _ in range(0, 5 + (rand::random::<u32>() % 10)) {
-        result.push_str(thread_rng().choose(&ZALGO_CHARS).unwrap().to_string().as_slice());
-      }
-    }
-
-    result
   }
 }
 
